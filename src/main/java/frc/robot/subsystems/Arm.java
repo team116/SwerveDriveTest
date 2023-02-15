770de14719375afdc6f4b60c4f127b2a7b45b687
ftc519 <ftc519@gmail.com>
2023-02-14 21:23:22 -0500

    Has most of the methods for arm and grabber

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,7 +1,48 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.sensors.CANCoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase{
-    
+    private CANSparkMax armMotor;
+    private CANCoder armCanCoder;
+
+    public Arm(int armCanID, int armCanCoderID){
+        armMotor = new CANSparkMax(armCanID, MotorType.kBrushless);
+        armCanCoder = new CANCoder(armCanCoderID);
+    }
+
+    public void moveUp(){
+        armMotor.set(0.02);
+    }
+
+    public void stop(){
+        armMotor.set(0);
+    }
+
+    public void moveDown(){
+        armMotor.set(-0.02);
+    }
+
+    public void highGoal(){
+        
+    }
+
+    public void midGoal(){
+
+    }
+
+    public void lowestPoint(){
+
+    }
+
+    public void humanPlayerStation(){
+
+    }
+
+
+
 }
