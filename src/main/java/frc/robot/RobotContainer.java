499dbdd97e9db5b6f0914b41ff6a633e1afbe413
ftc519 <ftc519@gmail.com>
2023-02-27 19:07:36 -0500

    useless imports thank you vscode

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,6 @@
 
 package frc.robot;
 
-import java.util.function.BooleanSupplier;
-
-import org.opencv.core.Point;
-
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
