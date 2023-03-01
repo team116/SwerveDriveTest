af7522c12189618ac0fe7c991930d33cac397730
ftc519 <ftc519@gmail.com>
2023-02-28 21:47:59 -0500

    specific constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,28 +8,35 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import frc.lib.config.SwerveModuleConstants;
 
+import static frc.robot.RobotSpecificConstants.getFrontToBackAxleToAxleMeters;
+import static frc.robot.RobotSpecificConstants.getSideToSideTreadCenterToTreadCenterMeters;
+import static frc.robot.RobotSpecificConstants.getAngleOffsetModule0;
+import static frc.robot.RobotSpecificConstants.getAngleOffsetModule1;
+import static frc.robot.RobotSpecificConstants.getAngleOffsetModule2;
+import static frc.robot.RobotSpecificConstants.getAngleOffsetMod3;
+
 public final class Constants {
 
   public static final class Swerve {
-    public static final double stickDeadband = 0.1;
+    public static final double STICK_DEADBAND = 0.1;
 
-    public static final int pigeonID = 7;
-    public static final int powerDistributionCenter = 6;
-    public static final boolean invertGyro = false; // Always ensure Gyro is CCW+ CW-
+    public static final int PIGEON_ID = 7;
+    public static final int POWER_DISTRIBUTION_CENTER = 6;
+    public static final boolean INVERT_GYRO = false; // Always ensure Gyro is CCW+ CW-
 
     /* Drivetrain Constants */
-    public static final double FRONT_TO_BACK_AXLE_TO_AXLE_METERS = Units.inchesToMeters(27.25);
-    public static final double SIDE_TO_SIDE_TREAD_CENTER_TO_TREAD_CENTER_METERS = Units.inchesToMeters(27.25);
-    public static final double wheelDiameter = Units.inchesToMeters(4.0);
-    public static final double wheelCircumference = wheelDiameter * Math.PI; //12.56637
+    public static final double FRONT_TO_BACK_AXLE_TO_AXLE_METERS = getFrontToBackAxleToAxleMeters();
+    public static final double SIDE_TO_SIDE_TREAD_CENTER_TO_TREAD_CENTER_METERS = getSideToSideTreadCenterToTreadCenterMeters();
+    public static final double WHEEL_DIAMETER = Units.inchesToMeters(4.0);
+    public static final double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI; //12.56637
 
-    public static final double openLoopRamp = 0.25;
-    public static final double closedLoopRamp = 0.0;
+    public static final double OPEN_LOOP_RAMP = 0.25;
+    public static final double CLOSED_LOOP_RAMP = 0.0;
 
-    public static final double driveGearRatio = (6.75 / 1.0); // original value 6.75
-    public static final double angleGearRatio = (150.0 / 7.0);
+    public static final double DRIVE_GEAR_RATIO = (6.75 / 1.0); // original value 6.75
+    public static final double ANGLE_GEAR_RATIO = (150.0 / 7.0);
 
-    public static final SwerveDriveKinematics swerveKinematics =
+    public static final SwerveDriveKinematics SWERVE_KINEMATICS =
         new SwerveDriveKinematics(
             new Translation2d(SIDE_TO_SIDE_TREAD_CENTER_TO_TREAD_CENTER_METERS / 2.0, FRONT_TO_BACK_AXLE_TO_AXLE_METERS / 2.0),
             new Translation2d(SIDE_TO_SIDE_TREAD_CENTER_TO_TREAD_CENTER_METERS / 2.0, -FRONT_TO_BACK_AXLE_TO_AXLE_METERS / 2.0),
@@ -37,105 +44,106 @@ public final class Constants {
             new Translation2d(-SIDE_TO_SIDE_TREAD_CENTER_TO_TREAD_CENTER_METERS / 2.0, -FRONT_TO_BACK_AXLE_TO_AXLE_METERS / 2.0));
 
     /* Swerve Voltage Compensation */
-    public static final double voltageComp = 12.0;
+    public static final double VOLTAGE_COMP = 12.0;
 
     /* Swerve Current Limiting */
-    public static final int angleContinuousCurrentLimit = 20;
-    public static final int driveContinuousCurrentLimit = 80;
+    public static final int ANGLE_CONTINUOUS_CURRENT_LIMIT = 20;
+    public static final int DRIVE_CONTINUOUS_CURRENT_LIMIT = 80;
 
     /* Angle Motor PID Values */
-    public static final double angleKP = 0.01;
-    public static final double angleKI = 0.0;
-    public static final double angleKD = 0.0;
-    public static final double angleKFF = 0.0;
+    public static final double ANGLE_KP = 0.01;
+    public static final double ANGLE_KI = 0.0;
+    public static final double ANGLE_KD = 0.0;
+    public static final double ANGLE_KFF = 0.0;
 
     /* Drive Motor PID Values */
-    public static final double driveKP = 1.0; // original value 0.1 // good manual driving 0.3
-    public static final double driveKI = 0.0;
-    public static final double driveKD = 0.0;
-    public static final double driveKFF = 0.0;
+    public static final double DRIVE_KP = 1.0; // original value 0.1 // good manual driving 0.3
+    public static final double DRIVE_KI = 0.0;
+    public static final double DRIVE_KD = 0.0;
+    public static final double DRIVE_KFF = 0.0;
 
     /* Drive Motor Characterization Values */
-    public static final double driveKS = 0.667;
-    public static final double driveKV = 2.44;
-    public static final double driveKA = 0.27;
+    public static final double DRIVE_KS = 0.667;
+    public static final double DRIVE_KV = 2.44;
+    public static final double DRIVE_KA = 0.27;
 
     /* Drive Motor Conversion Factors */
-    public static final double driveConversionPositionFactor =
-        (wheelDiameter * Math.PI) / driveGearRatio;
-    public static final double driveConversionVelocityFactor = driveConversionPositionFactor / 60.0;
-    public static final double angleConversionFactor = 360.0 / angleGearRatio;
+    public static final double DRIVE_CONVERSION_POSITION_FACTOR =
+        (WHEEL_DIAMETER * Math.PI) / DRIVE_GEAR_RATIO;
+    public static final double DRIVE_CONVERSION_VELOCITY_FACTOR = DRIVE_CONVERSION_POSITION_FACTOR / 60.0;
+    public static final double ANGLE_CONVERSION_FACTOR = 360.0 / ANGLE_GEAR_RATIO;
 
     /* Swerve Profiling Values */
-    public static final double maxSpeed = 3.5; // meters per second orginal value 4.5 FIXME: the speed does not seen to be changing
-    public static final double maxAngularVelocity = 11.5;
+    public static final double MAX_SPEED = 3.5; // meters per second orginal value 4.5 FIXME: the speed does not seen to be changing
+    public static final double MAX_ANGULAR_VELOCITY = 11.5;
 
     /* Neutral Modes */
-    public static final IdleMode angleNeutralMode = IdleMode.kBrake;
-    public static final IdleMode driveNeutralMode = IdleMode.kBrake;
+    public static final IdleMode ANGLE_NEUTRAL_MODE = IdleMode.kBrake;
+    public static final IdleMode DRIVE_NEUTRAL_MODE = IdleMode.kBrake;
 
     /* Motor Inverts */
-    public static final boolean driveInvert = true;
-    public static final boolean angleInvert = true;
+    public static final boolean DRIVE_INVERT = true;
+    public static final boolean ANGLE_INVERT = true;
 
     /* Angle Encoder Invert */
-    public static final boolean canCoderInvert = false;
+    public static final boolean CAN_CODER_INVERT = false;
 
     /* Module Specific Constants */
     /* Front Left Module - Module 0 */
-    public static final class Mod0 {
-      public static final int driveMotorID = 32;
-      public static final int angleMotorID = 31;
-      public static final int canCoderID = 30;
-      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(22.939);
-      public static final SwerveModuleConstants constants =
-          new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
+    public static final class MOD_0 {
+      public static final int DRIVE_MOTOR_ID = 32;
+      public static final int ANGLE_MOTOR_ID = 31;
+      public static final int CAN_CODER_ID = 30;
+      public static final Rotation2d ANGLE_OFFSET = getAngleOffsetModule0();
+      public static final SwerveModuleConstants CONSTANTS =
+          new SwerveModuleConstants(DRIVE_MOTOR_ID, ANGLE_MOTOR_ID, CAN_CODER_ID, ANGLE_OFFSET);
     }
 
     /* Front Right Module - Module 1 */
-    public static final class Mod1 {
-      public static final int driveMotorID = 22;
-      public static final int angleMotorID = 21;
-      public static final int canCoderID = 20;
-      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(139.394531);
-      public static final SwerveModuleConstants constants =
-          new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
+    public static final class MOD_1 {
+      public static final int DRIVE_MOTOR_ID = 22;
+      public static final int ANGLE_MOTOR_ID = 21;
+      public static final int CAN_CODER_ID = 20;
+      public static final Rotation2d ANGLE_OFFSET = getAngleOffsetModule1();
+      public static final SwerveModuleConstants CONSTANTS =
+          new SwerveModuleConstants(DRIVE_MOTOR_ID, ANGLE_MOTOR_ID, CAN_CODER_ID, ANGLE_OFFSET);
     }
 
     /* Back Left Module - Module 2 */
-    public static final class Mod2 {
-      public static final int driveMotorID = 42;
-      public static final int angleMotorID = 41;
-      public static final int canCoderID = 40;
-      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(241.962891);
-      public static final SwerveModuleConstants constants =
-          new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
+    public static final class MOD_2 {
+      public static final int DRIVE_MOTOR_ID = 42;
+      public static final int ANGLE_MOTOR_ID = 41;
+      public static final int CAN_CODER_ID = 40;
+      public static final Rotation2d ANGLE_OFFSET = getAngleOffsetModule2();
+      public static final SwerveModuleConstants CONSTANTS =
+          new SwerveModuleConstants(DRIVE_MOTOR_ID, ANGLE_MOTOR_ID, CAN_CODER_ID, ANGLE_OFFSET);
     }
 
     /* Back Right Module - Module 3 */
-    public static final class Mod3 {
-      public static final int driveMotorID = 12;
-      public static final int angleMotorID = 11;
-      public static final int canCoderID = 10;
-      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(21.708984);
-      public static final SwerveModuleConstants constants =
-          new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
+    public static final class MOD_3 {
+      public static final int DRIVE_MOTOR_ID = 12;
+      public static final int ANGLE_MOTOR_ID = 11;
+      public static final int CAN_CODER_ID = 10;
+      public static final Rotation2d ANGLE_OFFSET = getAngleOffsetMod3();
+      public static final SwerveModuleConstants CONSTANTS =
+          new SwerveModuleConstants(DRIVE_MOTOR_ID, ANGLE_MOTOR_ID, CAN_CODER_ID, ANGLE_OFFSET);
     }
   }
 
   public static final class AutoConstants {
-    public static final double kMaxSpeedMetersPerSecond = 3.5; // original value 3
-    public static final double kMaxAccelerationMetersPerSecondSquared = 0.5; // original value 3
-    public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
-    public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
+    public static final double MAX_SPEED_METERS_PER_SECOND = 3.5; // original value 3
+    public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 0.5; // original value 3
+    public static final double MAX_ANGULAR_SPEED_RADIANS_PER_SECOND = Math.PI;
+    public static final double MAX_ANGULAR_SPEED_RADIANS_PER_SECOND_SQUARED = Math.PI;
 
-    public static final double kPXController = 1;
-    public static final double kPYController = 1;
-    public static final double kPThetaController = 1;
+    public static final double PX_CONTROLLER = 1;
+    public static final double PY_CONTROLLER = 1;
+    public static final double PTHETA_CONTROLLER = 1;
 
     // Constraint for the motion profilied robot angle controller
-    public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
+    public static final TrapezoidProfile.Constraints THETA_CONTROLLER_CONSTRAINTS =
         new TrapezoidProfile.Constraints(
-            kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
+            MAX_ANGULAR_SPEED_RADIANS_PER_SECOND, MAX_ANGULAR_SPEED_RADIANS_PER_SECOND_SQUARED);
   }
+
 }
---
+++ src/main/java/frc/robot/RobotSpecificConstants.java
@@ -0,0 +1,63 @@
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+
+public final class RobotSpecificConstants {
+    
+    public static final boolean IS_COMPETITION_ROBOT = false; // Wonder if we can test robot itself to determine this
+
+    public static double getFrontToBackAxleToAxleMeters() {
+        return Units.inchesToMeters(ROBOT_SPECIFIC_CONSTANTS.getFrontToBackAxleToAxleInches());
+    }
+
+    public static double getSideToSideTreadCenterToTreadCenterMeters() {
+        return Units.inchesToMeters(ROBOT_SPECIFIC_CONSTANTS.getSideToSideTreadCenterToTreadCenterInches());
+    }
+
+    public static Rotation2d getAngleOffsetModule0() {
+        return Rotation2d.fromDegrees(ROBOT_SPECIFIC_CONSTANTS.getAngleOffsetDegreesMod0());
+    }
+
+    public static Rotation2d getAngleOffsetModule1() {
+        return Rotation2d.fromDegrees(ROBOT_SPECIFIC_CONSTANTS.getAngleOffsetDegreesMod1());
+    }
+
+    public static Rotation2d getAngleOffsetModule2() {
+        return Rotation2d.fromDegrees(ROBOT_SPECIFIC_CONSTANTS.getAngleOffsetDegreesMod2());
+    }
+
+    public static Rotation2d getAngleOffsetMod3() {
+        return Rotation2d.fromDegrees(ROBOT_SPECIFIC_CONSTANTS.getAngleOffsetDegreesMod3());
+    }
+
+    private interface SpecificConstants {
+        double getFrontToBackAxleToAxleInches();
+        double getSideToSideTreadCenterToTreadCenterInches();
+        double getAngleOffsetDegreesMod0();
+        double getAngleOffsetDegreesMod1();
+        double getAngleOffsetDegreesMod2();
+        double getAngleOffsetDegreesMod3();
+    }
+
+    private static final class CompetitionRobot implements SpecificConstants {
+        public double getFrontToBackAxleToAxleInches() { return 24.875; }
+        public double getSideToSideTreadCenterToTreadCenterInches() { return 24.5; }
+        public double getAngleOffsetDegreesMod0() { return 0.0; }
+        public double getAngleOffsetDegreesMod1() { return 0.0; }
+        public double getAngleOffsetDegreesMod2() { return 0.0; }
+        public double getAngleOffsetDegreesMod3() { return 0.0; }
+    }
+
+    private static final class TestRobot implements SpecificConstants {
+        public double getFrontToBackAxleToAxleInches() { return 27.25; }
+        public double getSideToSideTreadCenterToTreadCenterInches() { return 27.25; }
+        public double getAngleOffsetDegreesMod0() { return 22.939; }
+        public double getAngleOffsetDegreesMod1() { return 139.394531; }
+        public double getAngleOffsetDegreesMod2() { return 241.962891; }
+        public double getAngleOffsetDegreesMod3() { return 21.708984; }
+    }
+
+    private static final SpecificConstants ROBOT_SPECIFIC_CONSTANTS = IS_COMPETITION_ROBOT ? new CompetitionRobot() : new TestRobot();
+
+}
