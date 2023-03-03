5bc90b96638b66316751336ca110307d4539e7c5
ftc519 <ftc519@gmail.com>
2023-03-02 21:28:45 -0500

    starting work on driving to position

--- src/main/java/frc/robot/autos/DriveToPositionCommand.java
+++ src/main/java/frc/robot/autos/DriveToPositionCommand.java
@@ -1,10 +1,31 @@
 package frc.robot.autos;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Swerve;
 
 /**
  * This should allow for attempting to drive to a specific position
  */
 public class DriveToPositionCommand extends CommandBase {
-   
+    private Swerve swerve;
+
+    public DriveToPositionCommand(Swerve swerveSubsystem){
+        swerve = swerveSubsystem;
+        addRequirements(swerveSubsystem);
+    }
+
+    @Override
+    public void initialize(){
+        swerve.resetRelativeEncoders();
+    }
+
+    @Override
+    public void execute(){
+        
+    }
+
+    @Override
+    public void end(boolean interrupted){
+
+    }
 }
